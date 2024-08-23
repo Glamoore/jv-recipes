6349631d894c5df545537ef96b34710003fcaddf
Reece Moore <reecemoore@192.168.1.113>
2024-08-23 11:39:35 +0100

    Established a connection pool]

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import java.net.URL;
+import static java.lang.Boolean.TRUE;
+import static org.hibernate.cfg.JdbcSettings.*;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class Database {
+    SessionFactory ss;
+    public static SessionFactory getSessionFactory(){
+            SessionFactory sessionFactory = new Configuration()
+                    // use H2 in-memory database
+                    .setProperty("hibernate.connection.url", "jdbc:h2:mem:db1")
+                    // default username / password
+                    .setProperty("hibernate.connection.username", "sa")
+                    .setProperty("hibernate.connection.password", "")
+                    // options to display SQL in console
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.format_sql", "true")
+                    .setProperty("hibernate.highlight_sql", "true")
+                    .setProperty("hibernate.agroal.maxSize", "20")
+                    .buildSessionFactory();
+
+            return sessionFactory;
+
+    };
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import org.hibernate.Hibernate;
+import org.hibernate.SessionFactory;
+
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
+        SessionFactory firstSession = Database.getSessionFactory();
+
+        if(firstSession != null) {
+            System.out.println("Schema exported successfully");
+        } else {
+            System.out.println("Failed to export the schema");
+        }
+
+        firstSession.close();
+
     }
+
 }
